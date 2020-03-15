cf54f1aa9da9685ae8e3191c065c18d43dac1a04
Tom <veniat.tom@gmail.com>
2020-03-15 19:19:01 +0100

    Fix out size

--- imagenet/models/my_resnet.py
+++ imagenet/models/my_resnet.py
@@ -36,7 +36,7 @@ class MyResNet(nn.Module):
                                        dilate=replace_stride_with_dilation[1])
 
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(512 * block.expansion, num_classes)
+        self.fc = nn.Linear(256 * block.expansion, num_classes)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
